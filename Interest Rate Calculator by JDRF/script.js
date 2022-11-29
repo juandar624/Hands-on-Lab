8a50fb98a9b83dab936e0240842b8fa69b931854
juandar624 <119435011+juandar624@users.noreply.github.com>
2022-11-29 12:20:31 -0500

    Add files via upload

---
+++ Interest Rate Calculator by JDRF/script.js
@@ -0,0 +1,23 @@
+//Compute Function: determines the interest rate over the specified amount of time based on the principal amount.
+function compute()
+{   var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    document.getElementById("result").innerHTML='If you deposit <span class="highlight">'+principal+'</span>,\<br\>at an interest rate of <span class="highlight">'+rate+'%</span>\<br\>You will receive an amount of <span class="highlight">'+interest+'</span>,\<br\>in the year <span class="highlight">'+year+'</span>.\<br\>'
+
+}
+//UpdateRate Function: reads the value of the range slider and displays it the <span> adjacent to the slider.
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
